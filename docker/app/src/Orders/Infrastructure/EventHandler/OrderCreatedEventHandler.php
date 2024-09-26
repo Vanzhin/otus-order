72b3fac51fc2cdc43d99e5b89c52f73c2e1796d1
nikolay vanzhin <nikolay.vanzhin@yandex.ru>
2024-09-26 08:04:04 +0500

    feat. amqp messenger started

--- docker/app/src/Orders/Infrastructure/EventHandler/OrderCreatedEventHandler.php
+++ docker/app/src/Orders/Infrastructure/EventHandler/OrderCreatedEventHandler.php
@@ -3,16 +3,21 @@
 namespace App\Orders\Infrastructure\EventHandler;
 
 use App\Orders\Domain\Event\OrderCreatedEvent;
+use App\Orders\Domain\Message\ExternalMessageToForward;
 use App\Orders\Domain\Repository\OrderRepositoryInterface;
 use App\Shared\Application\Event\EventHandlerInterface;
+use App\Shared\Application\Message\MessageBusInterface;
 use App\Shared\Application\Service\BillingServiceInterface;
 use App\Shared\Infrastructure\Api\Billing\VO\TransactionVO;
+use Symfony\Component\Messenger\Bridge\Amqp\Transport\AmqpStamp;
+use Symfony\Component\Messenger\Envelope;
 
 readonly class OrderCreatedEventHandler implements EventHandlerInterface
 {
     public function __construct(
         private BillingServiceInterface  $billingService,
         private OrderRepositoryInterface $orderRepository,
+        private MessageBusInterface $messageBus,
     )
     {
     }
@@ -22,6 +27,10 @@ readonly class OrderCreatedEventHandler implements EventHandlerInterface
         $order = $this->orderRepository->findOneById($event->orderId);
         $transactionVo = new TransactionVO($order->getSum(), $order->getId(), 'order');
         $this->billingService->withdrawFromAccount($transactionVo, $order->getUserId());
+        $message2 = new ExternalMessageToForward('order-created', $transactionVo->jsonSerialize());
+        $envelope = new Envelope($message2, [new AmqpStamp("#")]);
+
+        $this->messageBus->execute($envelope);
 
         return $order->getId();
     }
